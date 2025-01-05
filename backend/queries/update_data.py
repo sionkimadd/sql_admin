9790b906bd44f8f747172986d6b3d3d3cb667ae5
sionkim <skim14@academic.rrc.ca>
2025-01-05 00:42:36 -0600

    Update update_data.py

--- backend/queries/update_data.py
+++ backend/queries/update_data.py
@@ -14,7 +14,7 @@ class UpdateDataQuery:
             with self.db_engine.connect() as c:
                 with c.begin():
                     target_data = ', '.join([f"{column} = {value}" for column, value in zip(self.target_columns, self.target_values)])
-                    query = f"UPDATE {self.table_name} SET {target_data} WHERE {self.condition_column} = '{self.condition_value}'"
+                    query = f"UPDATE {self.table_name} SET {target_data} WHERE {self.condition_column} = {self.condition_value}"
                     c.execute(text(query))
 
             return "Succeed: Data Updated", query
